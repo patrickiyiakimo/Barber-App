5b37ab5f53fde6b5900d756320a3310594bee1c4
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2025-08-05 11:06:35 +0100

    first commit

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,46 @@
+// import 'package:barber_app/services/widget_support.dart';
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: [
+            Image.asset('assets/images/undraw_barber_utly.png'),
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0),
+              child: Text(
+                'Welcome to\nBarber App',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Text(
+                'Book appointments, explore trending styles,\nand connect with top barbers all in one place.\nYour grooming experience just got\nsmarter and easier.',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey[700], fontSize: 16.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/widget_support.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle headlineTextFieldStyle() {
+    return TextStyle(
+      color: Colors.blue[400],
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+    );
+  }
+}
